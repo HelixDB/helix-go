53916d890f3b0764459286d9c00ab5c4b4ec96e7
Juan Martin Zabala Ochoa <juanmarzabala8a@gmail.com>
2025-05-29 21:17:02 -0500

    basic functionality (needs to be tested)

---
+++ queries.go
@@ -0,0 +1,51 @@
+package helix
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type HelixInput map[string]interface{}
+
+type HelixResponse map[string]interface{}
+
+func (c *Client) Query(endpoint string, data HelixInput) (*HelixResponse, error) {
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal input data: %w", err)
+	}
+
+	url := fmt.Sprintf("%s:%i/query", c.BaseURL, c.Port, endpoint)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create request: %w", err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	// Authorization token in the future maybe?
+
+	res, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to send request: %w", err)
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read response body: %w", err)
+	}
+
+	if res.StatusCode < 200 || res.StatusCode >= 300 {
+		return nil, fmt.Errorf("HTTP error %d: %s", res.StatusCode, string(body))
+	}
+
+	var response HelixResponse
+	if err := json.Unmarshal(body, &response); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal response: %w", err)
+	}
+
+	return &response, nil
+}
